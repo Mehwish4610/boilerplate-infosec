3efbd1c0d868ff828448040d05a6a220b87e5871
Mehwish4610 <125643042+Mehwish4610@users.noreply.github.com>
2023-12-09 22:50:25 +0530

    git commit -a myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action:'deny'}));
 
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+const api = require("./server.js");
+app.use("/_api", api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
 
+app.listen((process.env.PORT || 3000), () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 3000}`);
+});
+
+module.exports = app;
 
 
 
@@ -56,6 +71,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+app.listen((process.env.PORT || 3000), () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 3000}`);
 });
