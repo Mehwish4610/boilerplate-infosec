7cc23041f5f571430d4437e247ec581ebdf6f11a
Mehwish4610 <125643042+Mehwish4610@users.noreply.github.com>
2023-12-09 23:01:36 +0530

    git commit myApp.js git commit package-lock.json git commit package.json

--- myApp.js
+++ myApp.js
@@ -4,19 +4,6 @@ const app = express();
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action:'deny'}));
 
-app.use(express.static("public"));
-app.disable("strict-transport-security");
-const api = require("./server.js");
-app.use("/_api", api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-app.listen((process.env.PORT || 3000), () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 3000}`);
-});
-
-module.exports = app;
 
 
 
