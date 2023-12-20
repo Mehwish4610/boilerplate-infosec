366fb79019259d5c155cac53fb8dcb0434ee4a71
Mehwish4610 <125643042+Mehwish4610@users.noreply.github.com>
2023-12-20 21:22:34 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -9,6 +9,8 @@ app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({ maxAge: 90 * 24 * 60 * 60, force: true }));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(helmet.dnsPrefetchControl({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ));
+
 
 
 
